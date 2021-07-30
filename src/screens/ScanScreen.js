a39633b0987ac842d1044849074e25f60e00c15f
kohjiaying <73278030+kohjiaying@users.noreply.github.com>
2021-07-31 02:36:11 +0800

    qr scanner - managed to open camera

--- src/screens/ScanScreen.js
+++ src/screens/ScanScreen.js
@@ -1,21 +1,55 @@
-import React from 'react'
 import Background from '../components/Background'
 import Logo from '../components/Logo'
 import Header from '../components/Header'
 import Paragraph from '../components/Paragraph'
 import Button from '../components/Button'
 import { Text } from 'react-native-paper'
-import { View,StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native';
 
-import CameraPermission from '../components/cameraPermission'
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function ScanScreen({navigation}) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <SafeAreaView style={{ flex: 1 }}>
-		<CameraPermission/>
-		
-     </SafeAreaView>
+	<View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
 
