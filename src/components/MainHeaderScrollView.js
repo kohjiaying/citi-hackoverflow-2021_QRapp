9f1095028bbf49a0236d4bb52ed9e23ce85d82ab
underthehai <haishanlim@gmail.com>
2021-08-04 01:44:47 +0800

    Add vouchers to database

--- src/components/MainHeaderScrollView.js
+++ src/components/MainHeaderScrollView.js
@@ -3,162 +3,172 @@ import { Text, Image, View, StyleSheet, ScrollView, Dimensions, TouchableOpacity
 import firebase from '../../database/firebaseDB.js'
 
 class MainHeaderScrollView extends Component {
-	
-	getStoreDatabase(){
+
+	getStoreDatabase() {
 		firebase.firestore().collection('vouchers').get()
-		.then(querySnapshot=> {
-			const results = []
-			querySnapshot.docs.map(documentSnapshot=> results.push(documentSnapshot.data()))
-			this.setState({isLoading: false, storeDatabase: results})})
-		.catch(err => console.error(err))
+			.then(querySnapshot => {
+				const results = []
+				querySnapshot.docs.map(documentSnapshot => results.push(documentSnapshot.data()))
+				this.setState({ isLoading: false, storeDatabase: results })
+			})
+			.catch(err => console.error(err))
 	}
-   
+
 	state = {
 		modalVisible: false,
-		user: {'email': 'janedoe@gmail.com' , 'name': 'Jane Doe', 'userid': '8731'},
+		user: { 'email': 'janedoe@gmail.com', 'name': 'Jane Doe', 'userid': '8731' },
 		isLoading: true,
 		storeDatabase: null,
-      	selectedItem: {'voucherName': '', 'voucherDesc': '', 'voucherPrice': '', 'voucherid': '', 'voucherImage': '', 'storeid': ''},
-	    tempStr: 'dummy1'
-   }
+		selectedItem: { 'voucherName': '', 'voucherDesc': '', 'voucherPrice': '', 'voucherid': '', 'voucherImage': '', 'storeid': '' },
+		tempStr: 'dummy1'
+	}
 
-  setModalVisible(visible) {
-    	this.setState({modalVisible: visible});
-  }
-  setSelectedItem(newItem) {
-    	this.setState({selectedItem: newItem});
-  }
-  
-  makeid() {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < 10; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
-   charactersLength));
-   }
-   return result.toString();
-}
-
-  
-  setcartitemid(selectedItem, user){
-	const str1 = user.userid;
-	const str2 = selectedItem.storeid;
-	const str3 = selectedItem.voucherid;
-	const str4 = this.makeid();
-	const combi = str1 + str2 + str3 + str4;
-	this.setState({tempStr: combi});
-  }
-  
-  handleAddToCart() {
-	  firebase.firestore()
-	  .collection('cart')
-	  .doc(this.state.tempStr)
-	  .set({
-		  itemid: this.state.tempStr,
-		  userid: this.state.user.userid,
-		  voucherid: this.state.selectedItem.voucherid,
-		  storeid: this.state.selectedItem.storeid
-	  }).catch(err => console.error(err))
-  }
-	  
-  onPressButton(selectedItem, user) {
-	  this.setcartitemid(selectedItem, user)
-	  this.handleAddToCart()
-      alert('Added to cart!')
-  }
-  
-
-  render() {
-	    const { isLoading, storeDatabase} = this.state
-		
-		while (isLoading) {
-		 this.getStoreDatabase()
-		 return <ActivityIndicator/>
+	setModalVisible(visible) {
+		this.setState({ modalVisible: visible });
+	}
+	setSelectedItem(newItem) {
+		this.setState({ selectedItem: newItem });
+	}
+	/* use this to show the path
+		initialisePath = (item) =>
+			firebase.firestore().collection('vouchers').add({
+	
+				link: require("../assets/images/mainHeaderScrollView/pottery.jpg"),
+				//link: require("../assets/images/mainHeaderScrollView/crab.jpg")
+			}
+			)
+	/**/
+	makeid() {
+		var result = '';
+		var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		var charactersLength = characters.length;
+		for (var i = 0; i < 10; i++) {
+			result += characters.charAt(Math.floor(Math.random() *
+				charactersLength));
 		}
-	  
-      	return (
-          	<View style={styles.mainContainer}>
-		    		<Text style = {styles.header}> Featured Deals Of The Day </Text>
+		return result.toString();
+	}
+
+
+	setcartitemid(selectedItem, user) {
+		const str1 = user.userid;
+		const str2 = selectedItem.storeid;
+		const str3 = selectedItem.voucherid;
+		const str4 = this.makeid();
+		const combi = str1 + str2 + str3 + str4;
+		this.setState({ tempStr: combi });
+	}
+
+	handleAddToCart() {
+		firebase.firestore()
+			.collection('cart')
+			.doc(this.state.tempStr)
+			.set({
+				itemid: this.state.tempStr,
+				userid: this.state.user.userid,
+				voucherid: this.state.selectedItem.voucherid,
+				storeid: this.state.selectedItem.storeid
+			}).catch(err => console.error(err))
+	}
+
+	onPressButton(selectedItem, user) {
+		this.setcartitemid(selectedItem, user)
+		this.handleAddToCart()
+		alert('Added to cart!')
+	}
+
+
+	render() {
+		const { isLoading, storeDatabase } = this.state
+
+		while (isLoading) {
+			this.getStoreDatabase()
+			//this.initialisePath();
+			return <ActivityIndicator />
+		}
+
+		return (
+			<View style={styles.mainContainer}>
+				<Text style={styles.header}> Featured Deals Of The Day </Text>
 				<FlatList
-				    horizontal={true}
+					horizontal={true}
 					data={this.state.storeDatabase}
-					renderItem={({item}) => (
-						<TouchableOpacity key = {item.voucherid} style = {styles.item} 
+					renderItem={({ item }) => (
+						<TouchableOpacity key={item.voucherid} style={styles.item}
 							onPress={() => {
-                    			this.setModalVisible(true);
-			              		this.setSelectedItem(item); 
+								this.setModalVisible(true);
+								this.setSelectedItem(item);
 								this.setcartitemid(item, this.state.user)
-                  }}>
-							<Image 
-								style = {styles.featuredLogo}
-								source={item.voucherImage}/>
-                    		<Text>{item.voucherName}</Text>
-                		</TouchableOpacity>
+							}}>
+							<Image
+								style={styles.featuredLogo}
+								source={item.voucherImage} />
+							<Text>{item.voucherName}</Text>
+						</TouchableOpacity>
 					)}
-				keyExtractor={item => item.voucherid}/>
-					
-					<Modal
-			          	animationType="slide"
-			          	transparent={true}
-			          	visible={this.state.modalVisible}
-			          	onRequestClose={() => {
-					    	this.setModalVisible(!this.state.modalVisible);
-					      }}>
-			          	<View style={styles.modalcontainer}>
+					keyExtractor={item => item.voucherid} />
+
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={this.state.modalVisible}
+					onRequestClose={() => {
+						this.setModalVisible(!this.state.modalVisible);
+					}}>
+					<View style={styles.modalcontainer}>
 						<View style={styles.modalcard}>
-						<Text style={{fontSize: 20, padding: 5}}>{this.state.selectedItem.voucherName}</Text>
-						<Image 
-								style = {styles.featuredLogo}
-								source={this.state.selectedItem.voucherImage}/>
-                		<Text style={{fontSize: 15, padding: 5}}>{this.state.selectedItem.voucherPrice}</Text>
-                		<Text style={{fontSize: 12, padding: 5}}>{this.state.selectedItem.voucherDesc}</Text>
-                		<Text style={styles.space}></Text>
-                		<TouchableOpacity onPress={() => {
-                    			this.onPressButton(this.state.selectedItem, this.state.user);
+							<Text style={{ fontSize: 20, padding: 5 }}>{this.state.selectedItem.voucherName}</Text>
+							<Image
+								style={styles.featuredLogo}
+								source={this.state.selectedItem.voucherImage} />
+							<Text style={{ fontSize: 15, padding: 5 }}>{this.state.selectedItem.voucherPrice}</Text>
+							<Text style={{ fontSize: 12, padding: 5 }}>{this.state.selectedItem.voucherDesc}</Text>
+							<Text style={styles.space}></Text>
+							<TouchableOpacity onPress={() => {
+								this.onPressButton(this.state.selectedItem, this.state.user);
 								this.setModalVisible(!this.state.modalVisible);
-						}}>
-                  			<View style={styles.button}>
-                    			<Text style={styles.buttonText}>Add to cart</Text>
-                  			</View>
-                		</TouchableOpacity>
-                  		<Text style={styles.space}></Text>
-						<Pressable
-							style={[styles.button, styles.buttonClose]}
-							onPress={() => {
-								this.setModalVisible(!this.state.modalVisible);
-							}}
-						>
-							<Text style={styles.textStyle}>Close</Text>
-						</Pressable>
+							}}>
+								<View style={styles.button}>
+									<Text style={styles.buttonText}>Add to cart</Text>
+								</View>
+							</TouchableOpacity>
+							<Text style={styles.space}></Text>
+							<Pressable
+								style={[styles.button, styles.buttonClose]}
+								onPress={() => {
+									this.setModalVisible(!this.state.modalVisible);
+								}}
+							>
+								<Text style={styles.textStyle}>Close</Text>
+							</Pressable>
 						</View>
-			          	</View>
-					</Modal>
+					</View>
+				</Modal>
 			</View>
-    )
-  }
+		)
+	}
 }
 export default MainHeaderScrollView
 
-const styles = StyleSheet.create ({
-	scrollView : {}, 
-	mainContainer : {
+const styles = StyleSheet.create({
+	scrollView: {},
+	mainContainer: {
 		marginTop: 10
-	}, 
+	},
 	header: {
 		fontSize: 20
 	},
-    item: {
-      padding: 5,
-	  width: 300,
-      height: 200,
-	  alignItems: 'center',
-	  
-   },
-   modalcontainer: {
+	item: {
+		padding: 5,
+		width: 300,
+		height: 200,
 		alignItems: 'center',
-        justifyContent: 'center',
-		flex: 1 
+
+	},
+	modalcontainer: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		flex: 1
 	},
 	modalcard: {
 		margin: 5,
@@ -169,8 +179,8 @@ const styles = StyleSheet.create ({
 		height: 500,
 		shadowColor: "#000",
 		shadowOffset: {
-		width: 100,
-		height: 100
+			width: 100,
+			height: 100
 		},
 		shadowOpacity: 0.25,
 		shadowRadius: 4,
@@ -178,27 +188,27 @@ const styles = StyleSheet.create ({
 		alignItems: 'center',
 	},
 	button: {
-    alignItems: 'center',
-    backgroundColor: '#560CCE',
-    padding: 10,
-	  width: '100%'
-    },
+		alignItems: 'center',
+		backgroundColor: '#560CCE',
+		padding: 10,
+		width: '100%'
+	},
 	buttonClose: {
-	  backgroundColor: "#560CCE",
-    },
-  buttonText: {
-    textAlign: 'center',
-    padding: 20,
-    color: 'white'
-  },
-  space: {
-    width: 20,
-    height: 20,
-  },
-  featuredLogo: {
-    width: 290,
-    height: 160,
-	borderColor: '#414757',
-    borderWidth: 1
-  },
+		backgroundColor: "#560CCE",
+	},
+	buttonText: {
+		textAlign: 'center',
+		padding: 20,
+		color: 'white'
+	},
+	space: {
+		width: 20,
+		height: 20,
+	},
+	featuredLogo: {
+		width: 290,
+		height: 160,
+		borderColor: '#414757',
+		borderWidth: 1
+	},
 })
